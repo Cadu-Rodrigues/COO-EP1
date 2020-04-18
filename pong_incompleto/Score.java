311a1e58347381cafcd0912b84babb32ae0a1459
Carlos Eduardo <carloseduardordesousa@gmail.com>
2020-04-18 20:40:40 -0300

    Adiciona score

--- pong_incompleto/Score.java
+++ pong_incompleto/Score.java
@@ -1,48 +1,55 @@
 import java.awt.*;
 
 /**
-	Esta classe representa um placar no jogo. A classe princial do jogo (Pong)
-	instancia dois objeto deste tipo, cada um responsável por gerenciar a pontuação
-	de um player, quando a execução é iniciada.
-*/
+ * Esta classe representa um placar no jogo. A classe princial do jogo (Pong)
+ * instancia dois objeto deste tipo, cada um responsável por gerenciar a
+ * pontuação de um player, quando a execução é iniciada.
+ */
 
 public class Score {
 
 	/**
-		Construtor da classe Score.
+	 * Construtor da classe Score.
+	 * 
+	 * @param playerId uma string que identifica o player ao qual este placar está
+	 *                 associado.
+	 */
+	private String playerId;
+	private int score;
 
-		@param playerId uma string que identifica o player ao qual este placar está associado.
-	*/
+	public Score(String playerId) {
+		this.playerId = playerId;
+		this.score = 0;
+	}
 
-	public Score(String playerId){
+	/**
+	 * Método de desenho do placar.
+	 */
+
+	public void draw() {
+		if (this.playerId == "Player 1")
+			GameLib.drawText(this.playerId + ": " + this.getScore(), 70, GameLib.ALIGN_LEFT);
+		if (this.playerId == "Player 2")
+			GameLib.drawText(this.playerId + ": " + this.getScore(), 70, GameLib.ALIGN_RIGHT);
+	}
+
+	/**
+	 * Método que incrementa em 1 unidade a contagem de pontos.
+	 */
+
+	public void inc() {
+		this.score++;
 
 	}
 
 	/**
-		Método de desenho do placar.
-	*/
+	 * Método que devolve a contagem de pontos mantida pelo placar.
+	 * 
+	 * @return o valor inteiro referente ao total de pontos.
+	 */
 
-	public void draw(){
+	public int getScore() {
 
-		GameLib.drawText("placar?", 70, GameLib.ALIGN_LEFT);			
-	}
-
-	/**
-		Método que incrementa em 1 unidade a contagem de pontos.
-	*/
-
-	public void inc(){
-
-	}
-
-	/**
-		Método que devolve a contagem de pontos mantida pelo placar.
-
-		@return o valor inteiro referente ao total de pontos.
-	*/
-
-	public int getScore(){
-
-		return 0;
+		return this.score;
 	}
 }
